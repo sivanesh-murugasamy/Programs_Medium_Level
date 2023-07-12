c0cf22efe071897f532eb55d46659bebaa97e3ff
sivanesh-murugasamy <88439516+sivanesh-murugasamy@users.noreply.github.com>
2023-07-12 21:36:45 +0530

    Add files via upload

---
+++ simplepy.py
@@ -0,0 +1,17 @@
+
+""" Reverse string in python """
+
+s1="Someprogram"
+print(s1[::-1]) 
+
+
+""" missing of array elements""" 
+"""using sum of n natural numbers Xor sum of array"""
+
+arr=[1,2,4,6,7]
+n=arr[-1]
+total=n*(n+1)//2
+s1=sum(arr)
+print(total-s1)
+
+
