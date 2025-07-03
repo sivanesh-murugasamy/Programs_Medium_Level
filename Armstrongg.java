87b78b740a6d7a3ad4909e63ede1b51043871217
Sivanesh M <88439516+sivanesh-murugasamy@users.noreply.github.com>
2025-07-03 23:23:53 +0530

    Add files via upload

---
+++ Armstrongg.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class Armstrongg
+{
+  public static void main(String argu[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    while(n!=0)
+    {
+      n--;
+      System.out.println(n);
+    }
+ }
+}
